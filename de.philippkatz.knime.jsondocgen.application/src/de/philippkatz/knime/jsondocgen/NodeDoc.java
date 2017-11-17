e122c5b8d90f24c8dc1b936a0f9863c796a8a12e
Philipp Katz <philipp@philippkatz.de>
2017-11-17 19:43:12 +0100

    Clean up code, use builder

--- de.philippkatz.knime.jsondocgen.application/src/de/philippkatz/knime/jsondocgen/NodeDoc.java
+++ de.philippkatz.knime.jsondocgen.application/src/de/philippkatz/knime/jsondocgen/NodeDoc.java
@@ -1,55 +1,193 @@
 package de.philippkatz.knime.jsondocgen;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
-/** Intermediate object used for constructing the JSON and for testing. */
+/**
+ * Intermediate object used for constructing the JSON and for testing. Use the
+ * {@link NodeDocBuilder} for construction.
+ */
 public final class NodeDoc {
+	
+	public static final class NodeDocBuilder {
+		private String identifier;
+		private String name;
+		private String shortDescription;
+		private String intro;
+		private List<OptionTab> optionTabs;
+		private List<Option> options;
+		private List<Port> inPorts;
+		private List<Port> outPorts;
+		private List<View> views;
+		private String icon;
+		private String type;
+		private boolean deprecated;
+		private InteractiveView interactiveView;
+		public NodeDocBuilder setIdentifier(String identifier) {
+			this.identifier = identifier;
+			return this;
+		}
+		public NodeDocBuilder setName(String name) {
+			this.name = name;
+			return this;
+		}
+		public NodeDocBuilder setShortDescription(String shortDescription) {
+			this.shortDescription = shortDescription;
+			return this;
+		}
+		public NodeDocBuilder setIntro(String intro) {
+			this.intro = intro;
+			return this;
+		}
+		public NodeDocBuilder setOptionTabs(List<OptionTab> optionTabs) {
+			this.optionTabs = optionTabs;
+			return this;
+		}
+		public NodeDocBuilder setOptions(List<Option> options) {
+			this.options = options;
+			return this;
+		}
+		public NodeDocBuilder setInPorts(List<Port> inPorts) {
+			this.inPorts = inPorts;
+			return this;
+		}
+		public NodeDocBuilder setOutPorts(List<Port> outPorts) {
+			this.outPorts = outPorts;
+			return this;
+		}
+		public NodeDocBuilder setViews(List<View> views) {
+			this.views = views;
+			return this;
+		}
+		public NodeDocBuilder setIcon(String icon) {
+			this.icon = icon;
+			return this;
+		}
+		public NodeDocBuilder setType(String type) {
+			this.type = type;
+			return this;
+		}
+		public NodeDocBuilder setDeprecated(boolean deprecated) {
+			this.deprecated = deprecated;
+			return this;
+		}
+		public NodeDocBuilder setInteractiveView(InteractiveView interactiveView) {
+			this.interactiveView = interactiveView;
+			return this;
+		}
+		public NodeDocBuilder addOptionTab(OptionTab optionTab) {
+			if (optionTabs == null) {
+				optionTabs = new ArrayList<>();
+			}
+			optionTabs.add(optionTab);
+			return this;
+		}
+		public NodeDocBuilder addView(View view) {
+			if (views == null) {
+				views = new ArrayList<>();
+			}
+			views.add(view);
+			return this;
+		}
+		public NodeDoc build() {
+			return new NodeDoc(this);
+		}
+	}
+	
 	public static final class OptionTab {
-		String name;
-		List<Option> options;
-		OptionTab() { /* package private */ }
+		final String name;
+		final List<Option> options;
+		/* package */ OptionTab(String name, List<Option> options) {
+			this.name = name;
+			this.options = options;
+		}
 	}
 	public static final class Option {
-		String name;
-		String description;
-		boolean optional;
-		Option() { /* package private */ }
+		final String name;
+		final String description;
+		final boolean optional;
+		/* package */ Option(String name, String description, boolean optional) {
+			this.name = name;
+			this.description = description;
+			this.optional = optional;
+		}
 	}
 	public static final class Port {
-		int index;
-		String name;
-		String description;
+		final int index;
+		final String name;
+		final String description;
 		/** null in case of an outputPort. */
-		Boolean optional;
-		Port() { /* package private */ }
+		final Boolean optional;
+		/* package */ Port(int index, String name, String description, Boolean optional) {
+			this.index = index;
+			this.name = name;
+			this.description = description;
+			this.optional = optional;
+		}
 	}
 	public static final class View {
-		int index;
-		String name;
-		String description;
-		View() { /* package private */ }
+		final int index;
+		final String name;
+		final String description;
+		/* package */ View(int index, String name, String description) {
+			this.index = index;
+			this.name = name;
+			this.description = description;
+		}
 	}
 	public static final class InteractiveView {
-		String name;
-		String description;
+		final String name;
+		final String description;
+		/* package */ InteractiveView(String name, String description) {
+			this.name = name;
+			this.description = description;
+		}
 	}
-	String identifier;
-	String name;
-	String shortDescription;
-	String intro;
-	List<OptionTab> optionTabs;
-	List<Option> options;
-	List<Port> inPorts;
-	List<Port> outPorts;
-	List<View> views;
-	String icon;
-	String type;
-	boolean deprecated;
-	InteractiveView interactiveView;
-	NodeDoc(){ /* package private */ }
+
+	final String identifier;
+	final String name;
+	final String shortDescription;
+	final String intro;
+	final List<OptionTab> optionTabs;
+	final List<Option> options;
+	final List<Port> inPorts;
+	final List<Port> outPorts;
+	final List<View> views;
+	final String icon;
+	final String type;
+	final boolean deprecated;
+	final InteractiveView interactiveView;
+
+	private NodeDoc(NodeDocBuilder builder) {
+		identifier = builder.identifier;
+		name = builder.name;
+		shortDescription = builder.shortDescription;
+		intro = builder.intro;
+		optionTabs = copyOrNull(builder.optionTabs);
+		options = copyOrNull(builder.options);
+		inPorts = copyOrNull(builder.inPorts);
+		outPorts = copyOrNull(builder.outPorts);
+		views = copyOrNull(builder.views);
+		icon = builder.icon;
+		type = builder.type;
+		deprecated = builder.deprecated;
+		interactiveView = builder.interactiveView;
+	}
+
+	/**
+	 * Create a copy of the given list, return <code>null</code> when argument is
+	 * <code>null</code>.
+	 * 
+	 * @param list
+	 *            The list to copy.
+	 * @return A copy of the list or <code>null</code>.
+	 */
+	private static <T> List<T> copyOrNull(List<T> list) {
+		return list != null ? new ArrayList<>(list) : null;
+	}
 
 	public String toJson() {
 		Gson gson = new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create();
--- de.philippkatz.knime.jsondocgen.application/src/de/philippkatz/knime/jsondocgen/NodeDocJsonParser.java
+++ de.philippkatz.knime.jsondocgen.application/src/de/philippkatz/knime/jsondocgen/NodeDocJsonParser.java
@@ -15,6 +15,7 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Node;
 
 import de.philippkatz.knime.jsondocgen.NodeDoc.InteractiveView;
+import de.philippkatz.knime.jsondocgen.NodeDoc.NodeDocBuilder;
 import de.philippkatz.knime.jsondocgen.NodeDoc.Option;
 import de.philippkatz.knime.jsondocgen.NodeDoc.OptionTab;
 import de.philippkatz.knime.jsondocgen.NodeDoc.Port;
@@ -40,60 +41,53 @@ public final class NodeDocJsonParser {
 		domBuilder.enableXmlMode();
 		Jerry jerry = jerryParser.parse(xmlString);
 
-		NodeDoc nodeDoc = new NodeDoc();
+		NodeDocBuilder builder = new NodeDocBuilder();
 
-		nodeDoc.name = trim(jerry.$("knimeNode name").text());
-		nodeDoc.shortDescription = trim(jerry.$("knimeNode shortDescription").text());
-		nodeDoc.intro = trim(jerry.$("knimeNode fullDescription intro").html());
-		nodeDoc.identifier = nodeIdentifier;
-		nodeDoc.icon = jerry.$("knimeNode").attr("icon");
-		nodeDoc.type = jerry.$("knimeNode").attr("type");
-		nodeDoc.deprecated = parseOptionalBoolean(jerry.$("knimeNode").attr("deprecated"));
+		builder.setName(trim(jerry.$("knimeNode name").text()));
+		builder.setShortDescription(trim(jerry.$("knimeNode shortDescription").text()));
+		builder.setIntro(trim(jerry.$("knimeNode fullDescription intro").html()));
+		builder.setIdentifier(nodeIdentifier);
+		builder.setIcon(jerry.$("knimeNode").attr("icon"));
+		builder.setType(jerry.$("knimeNode").attr("type"));
+		builder.setDeprecated(parseOptionalBoolean(jerry.$("knimeNode").attr("deprecated")));
 
 		// options are either children of fullDescription,
 		// or nested within tab elements
 		Jerry tabs = jerry.$("knimeNode fullDescription tab");
 		if (tabs.length() > 0) {
-			List<OptionTab> optionTabs = new ArrayList<>();
 			for (Jerry tab : tabs) {
-				OptionTab optionTab = new OptionTab();
-				optionTab.name = tab.attr("name");
-				optionTab.options = parseOptions(tab.$("option"));
-				optionTabs.add(optionTab);
+				String name = tab.attr("name");
+				List<Option> options = parseOptions(tab.$("option"));
+				builder.addOptionTab(new OptionTab(name, options));
 			}
-			nodeDoc.optionTabs = optionTabs;
 		} else {
-			nodeDoc.options = parseOptions(jerry.$("knimeNode fullDescription option"));
+			builder.setOptions(parseOptions(jerry.$("knimeNode fullDescription option")));
 		}
 
 		// ports
-		nodeDoc.inPorts = parsePorts(jerry.$("knimeNode ports inPort"), true);
-		nodeDoc.outPorts = parsePorts(jerry.$("knimeNode ports outPort"), false);
+		builder.setInPorts(parsePorts(jerry.$("knimeNode ports inPort"), true));
+		builder.setOutPorts(parsePorts(jerry.$("knimeNode ports outPort"), false));
 
 		// views
 		Jerry views = jerry.$("knimeNode views view");
 		if (views.length() > 0) {
-			List<View> viewObjects = new ArrayList<>();
 			for (Jerry view : views) {
-				View viewObject = new View();
-				viewObject.index = Integer.valueOf(view.attr("index"));
-				viewObject.name = view.attr("name");
-				viewObject.description = trim(view.html());
-				viewObjects.add(viewObject);
+				int index = Integer.valueOf(view.attr("index"));
+				String name = view.attr("name");
+				String description = trim(view.html());
+				builder.addView(new View(index, name, description));
 			}
-			nodeDoc.views = viewObjects;
 		}
 		
 		// interactive view
 		Jerry interactiveView = jerry.$("knimeNode interactiveView");
 		if (interactiveView.size() > 0) {
-			InteractiveView interactiveViewObject = new InteractiveView();
-			interactiveViewObject.name = interactiveView.attr("name");
-			interactiveViewObject.description = trim(interactiveView.html());
-			nodeDoc.interactiveView = interactiveViewObject;
+			String name = interactiveView.attr("name");
+			String description = trim(interactiveView.html());
+			builder.setInteractiveView(new InteractiveView(name, description));
 		}
-		
-		return nodeDoc;
+
+		return builder.build();
 
 	}
 
@@ -104,11 +98,10 @@ public final class NodeDocJsonParser {
 	private static List<Option> parseOptions(Jerry options) {
 		List<Option> optionsJson = new ArrayList<>();
 		for (Jerry option : options) {
-			Option optionObject = new Option();
-			optionObject.name = option.attr("name");
-			optionObject.description = trim(option.html());
-			optionObject.optional = parseOptionalBoolean(option.attr("optional"));
-			optionsJson.add(optionObject);
+			String name = option.attr("name");
+			String description = trim(option.html());
+			boolean optional = parseOptionalBoolean(option.attr("optional"));
+			optionsJson.add(new Option(name, description, optional));
 		}
 		return optionsJson;
 	}
@@ -120,14 +113,14 @@ public final class NodeDocJsonParser {
 	private static List<Port> parsePorts(Jerry ports, boolean isInPort) {
 		List<Port> portsJson = new ArrayList<>();
 		for (Jerry port : ports) {
-			Port portObject = new Port();
-			portObject.index = Integer.valueOf(port.attr("index"));
-			portObject.name = port.attr("name");
-			portObject.description = trim(port.html());
+			int index = Integer.valueOf(port.attr("index"));
+			String name = port.attr("name");
+			String description = trim(port.html());
+			Boolean optional = null;
 			if (isInPort) {
-				portObject.optional = parseOptionalBoolean(port.attr("optional"));
+				optional = parseOptionalBoolean(port.attr("optional"));
 			}
-			portsJson.add(portObject);
+			portsJson.add(new Port(index, name, description, optional));
 		}
 		return portsJson;
 	}
